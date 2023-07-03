7a8440ef45b1c73ec7b744e21d50d514f76b3b33
aldrin.almia <aldrin.almia@CBEN-G40MGS3-LX>
2023-07-03 20:28:11 +0800

    Add project files.

---
+++ AccentureWatch/Controllers/WatchesController.cs
@@ -0,0 +1,42 @@
+﻿using AccentureWatch.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace AccentureWatch.Controllers
+{
+    public class WatchesController : Controller
+    {
+
+
+        public async Task<IActionResult> Index()
+        {
+            string? _GetWatchApiUrl = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("AppSettings")["GetWatchApiUrl"];
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(_GetWatchApiUrl))
+                {
+                    string? apiResponse = await response.Content.ReadAsStringAsync();
+                    var list = JsonConvert.DeserializeObject<List<Watch>>(apiResponse);
+                    return View(list);
+                }
+            }
+
+        }
+
+        [HttpGet]
+        public IActionResult Add(Watch watch)
+        {
+            return View();
+
+        }
+
+        [HttpPost, ActionName("Add")]
+        public IActionResult AddWatch(Watch watch)
+        {
+            return View();
+
+        }
+
+    }
+}
---
+++ AccentureWatch/Models/Watch.cs
@@ -0,0 +1,91 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
+using DataAnnotationsExtensions;
+
+namespace AccentureWatch.Models
+{
+    public class Watch
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Display(Name = "Item Name")]
+        [Required(ErrorMessage = "Required")]
+        [StringLength(50)]
+        public string? ItemName { get; set; }
+
+        [Display(Name = "Item Number")]
+        [Required(ErrorMessage = "Required")]
+        [StringLength(50)]
+        public string? ItemNumber { get; set; }
+
+        [Display(Name = "Short Description")]
+        [Required(ErrorMessage = "Required")]
+        [StringLength(50)]
+        public string? ShortDescription { get; set; }
+
+        [Display(Name = "Full Description")]
+        [Required(ErrorMessage = "Required")]
+        [StringLength(100)]
+        public string? FullDescription { get; set; }
+
+        [RegularExpression(@"-?\d+(?:\.\d+)?", ErrorMessage = "Please enter valid price (e.g. 20.00)")]
+        [Required(ErrorMessage = "Required")]
+        public decimal? Price { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [StringLength(50)]
+        public string? Caliber { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [StringLength(50)]
+        public string? Movement { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [StringLength(50)]
+        public string? Chronograph { get; set; }
+
+        [Display(Name = "Weight (mm)")]
+        [RegularExpression(@"-?\d+(?:\.\d+)?", ErrorMessage = "Please enter valid weight (e.g. 20.00)")]
+        [Required(ErrorMessage = "Required")]
+        public decimal? Weight { get; set; }
+
+        [Display(Name = "Height (mm)")]
+        [RegularExpression(@"-?\d+(?:\.\d+)?", ErrorMessage = "Please enter valid height (e.g. 20.00)")]
+        [Required(ErrorMessage = "Required")]
+        public decimal? Height { get; set; }
+
+        [Display(Name = "Diameter (mm)")]
+        [RegularExpression(@"-?\d+(?:\.\d+)?", ErrorMessage = "Please enter valid diameter (e.g. 20.00)")]
+        [Required(ErrorMessage = "Required")]
+        public decimal? Diameter { get; set; }
+
+        [Display(Name = "Thickness (mm)")]
+        [RegularExpression(@"-?\d+(?:\.\d+)?", ErrorMessage = "Please enter valid thickness (e.g. 20.00)")]
+        [Required(ErrorMessage = "Required")]
+        public decimal? Thickness { get; set; }
+
+        [Integer(ErrorMessage = "Please enter numbers only")]
+        [Required(ErrorMessage = "Required")]
+        public int? Jewel { get; set; }
+
+        [Display(Name = "Case Material")]
+        [Required(ErrorMessage = "Required")]
+        [StringLength(30)]
+        public string? CaseMaterial { get; set; }
+
+        [Display(Name = "Strap Material")]
+        [Required(ErrorMessage = "Required")]
+        [StringLength(30)]
+        public string? StrapMaterial { get; set; }
+
+        //[NotMapped]
+        [Display(Name = "File")]
+        public IFormFile? FormFile { get; set; }
+
+        public string? URL { get; set; }
+    }
+}
